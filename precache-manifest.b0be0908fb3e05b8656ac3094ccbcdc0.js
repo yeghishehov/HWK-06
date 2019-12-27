4f7fb745aea4d15ec5aab5054d34de47f2dc22c2
yeghishehov <yeghishehov.1@gmail.com>
2019-12-27 14:11:44 +0400

    Updates

---
+++ precache-manifest.b0be0908fb3e05b8656ac3094ccbcdc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fc4e1ec5500e9436e55ff39bf8d15a",
+    "url": "/HWK-06/index.html"
+  },
+  {
+    "revision": "f39529b76aac15f9be08",
+    "url": "/HWK-06/static/css/main.f5fed4cd.chunk.css"
+  },
+  {
+    "revision": "8448939bca95d22b7453",
+    "url": "/HWK-06/static/js/2.c6f1d1ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/HWK-06/static/js/2.c6f1d1ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f39529b76aac15f9be08",
+    "url": "/HWK-06/static/js/main.b10915ca.chunk.js"
+  },
+  {
+    "revision": "e76e846ac2c351e3681a",
+    "url": "/HWK-06/static/js/runtime-main.5ef4a4c5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/HWK-06/static/media/logo.ee7cd8ed.svg"
+  }
+]);
